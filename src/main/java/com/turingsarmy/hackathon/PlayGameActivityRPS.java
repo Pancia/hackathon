8403b0c6d0bbb9ead2c816b043f45f6194b3abb8
jahyman <jahyman@ucsc.edu>
2014-04-05 15:20:33 -0700

    Progress thus far
    RPS expanded.

--- src/main/java/com/turingsarmy/hackathon/PlayGameActivityRPS.java
+++ src/main/java/com/turingsarmy/hackathon/PlayGameActivityRPS.java
@@ -4,13 +4,51 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PlayGameActivityRPS extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_rps);
+
+        //Display Names
+        final String p1Name = "Jeff";
+        TextView tvUsername = (TextView) findViewById(R.id.activity_rps_p1);
+        tvUsername.setText(p1Name);
+        final String p2Name = "Sally";
+        TextView tvEnemyName = (TextView) findViewById(R.id.activity_rps_p2);
+        tvEnemyName.setText(p2Name);
+
+        //Buttons
+        final Button rock = (Button) findViewById(R.id.activity_rps_rock);
+        final Button paper = (Button) findViewById(R.id.activity_rps_paper);
+        final Button scissor = (Button) findViewById(R.id.activity_rps_scissor);
+        final Button submit = (Button) findViewById(R.id.activity_rps_submit);
+
+        rock.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Throw Rock
+            }
+        });
+        paper.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Throw Paper
+            }
+        });
+        scissor.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Throw Scissor
+            }
+        });
+        submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Submit toss
+            }
+        });
     }
 
     @Override
