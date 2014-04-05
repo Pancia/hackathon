a4b859f0e2fc65a2154814bd2392c42c66f2a77f
wc_karatekid <rtanase@ucsc.edu>
2014-04-05 16:06:18 -0700

    Finalized Signup Page
    -Finished Signup Page (NEEDS TO CHECK IF VALID UCSC EMAIL)
    -Goes to signup page when signup button is clicked (Yay!)
    
    -TODO fix text edits, they don't delete the word username or password
    when you start typing

--- src/main/java/com/turingsarmy/hackathon/MainActivity.java
+++ src/main/java/com/turingsarmy/hackathon/MainActivity.java
@@ -1,16 +1,42 @@
 package com.turingsarmy.hackathon;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Resources res = getResources();
         setContentView(R.layout.activity_main);
+
+        final Button login = (Button) findViewById(R.id.activitymain_button_login);
+        final Button signup = (Button) findViewById(R.id.activitymain_button_signup);
+        final EditText username = (EditText) findViewById(R.id.activitymain_edittext_username);
+        final EditText password = (EditText) findViewById(R.id.activitymain_edittext_password);
+
+        login.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v)
+            {
+                //perform action
+            }
+        });
+
+        signup.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent myIntent = new Intent(MainActivity.this, SignupActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 
     @Override
---
+++ src/main/java/com/turingsarmy/hackathon/SignupActivity.java
@@ -0,0 +1,45 @@
+package com.turingsarmy.hackathon;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SignupActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Resources res = getResources();
+        setContentView(R.layout.signup);
+
+        Spinner s = (Spinner) findViewById(R.id.signup_spinner_college);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item, res.getStringArray(R.array.signup_colleges));
+        s.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
