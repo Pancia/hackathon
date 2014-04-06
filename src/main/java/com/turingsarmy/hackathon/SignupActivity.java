37a3a12b69dd68ed9528c914152610828f76f309
wc_karatekid <rtanase@ucsc.edu>
2014-04-05 17:42:32 -0700

    Updated signup activity to check for valid email

--- src/main/java/com/turingsarmy/hackathon/SignupActivity.java
+++ src/main/java/com/turingsarmy/hackathon/SignupActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SignupActivity extends ActionBarActivity {
 
@@ -16,11 +20,31 @@ public class SignupActivity extends ActionBarActivity {
         Resources res = getResources();
         setContentView(R.layout.signup);
 
-        Spinner s = (Spinner) findViewById(R.id.signup_spinner_college);
+        final Spinner collegeSpinner = (Spinner) findViewById(R.id.signup_spinner_college);
+        final Button submit = (Button) findViewById(R.id.signup_button_submit);
+        final EditText username = (EditText) findViewById(R.id.signup_edittext_username);
+        final EditText password = (EditText) findViewById(R.id.signup_edittext_password);
+        final EditText email = (EditText) findViewById(R.id.signup_edittext_email);
         ArrayAdapter adapter = new ArrayAdapter(this,
                 R.layout.row, R.id.row_textview_spinnerelement, res.getStringArray(R.array.signup_colleges));
-        s.setAdapter(adapter);
-    }
+        collegeSpinner.setAdapter(adapter);
+
+        submit.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                if (!email.getText().toString().isEmpty() && !password.getText().toString().isEmpty() && !username.toString().isEmpty()){
+                    String userEmail = email.getText().toString();
+                    if (userEmail.contains("@ucsc.edu")){
+                        Toast.makeText(getApplicationContext(), "Good Job", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Please enter a valid UCSC email", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
