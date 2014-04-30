0986d54600c81b944f8d352ab8ef05471f6d48fa
wc_karatekid <rtanase@ucsc.edu>
2014-04-30 02:19:20 -0700

    Made Master Mind Fragment .xml and .java (shell)
    -Still needs to add functionality

--- src/main/java/com/turingsarmy/hackathon/PlayMMFragment.java
+++ src/main/java/com/turingsarmy/hackathon/PlayMMFragment.java
@@ -1,10 +1,38 @@
 package com.turingsarmy.hackathon;
 
-public class PlayMMFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlayMMFragment extends Fragment {
 
     private static final String TAG = PlayMMFragment.class.getSimpleName();
+    private String home;
 
-    public PlayMMFragment() {}
+    public PlayMMFragment(String home) {
+        this.home = home;
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.playmm_fragment, container, false);
+        assert v != null;
+        if (savedInstanceState != null){
+            home = savedInstanceState.getString("home");
+        }
+        setupGame(v, home);
+        return v;
+    }
 
+    private void setupGame(View v, String home){
+        //
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        outState.putString("home", home);
+        super.onSaveInstanceState(outState);
+    }
 }
